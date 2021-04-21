7e78841776b99b6e7c18c4f3a1bd6cdbb054bff5
kuldeepraw414 <kuldeeprawat.kr@gmail.com>
2021-04-21 13:43:29 +0530

    Setup basic files

---
+++ components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Image from 'next/image'
+
+function Header() {
+    return (
+        <header>
+            <Image src="https://links.papareact.com/ua6" className="object-contain" width={200} height={100} />
+        </header>
+    )
+}
+
+export default Header
